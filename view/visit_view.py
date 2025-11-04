5ba417379a021c504b7bba1657baaef8cdbb782e
fatemehahang <fatemehahang484@gmail.com>
2025-11-04 14:31:19 +0330

    update

--- view/visit_view.py
+++ view/visit_view.py
@@ -10,7 +10,7 @@ from view.component.table import Table
 class Visit:
     def __init__(self):
         self.window = Tk()
-        self.window.geometry("300x300")
+        self.window.geometry("500x500")
         self.window.title("Doctor Visit")
 
         self.patient_id = LabelWithEntry(self.window, "id", 20, 20, data_type=IntVar, state="readonly")
@@ -21,7 +21,7 @@ class Visit:
         self.date_time = LabelWithEntry(self.window, "DateTime", 20, 220)
         self.description = LabelWithEntry(self.window, "Description", 20, 260)
 
-        self.tabel = Table(
+        self.table = Table(
             self.window,
             ["Id", "first_name", "last_name", "phone_number", "doctor_name", "date_time", "description"],
             [40,100,100,100,100,160],
@@ -30,8 +30,7 @@ class Visit:
             self.select_from_table
         )
 
-        Button(self.window, text="Select patient", width=19, command=self.select_visit).place(x=20, y=220)
-        Button(self.window, text="Refresh", width=7, command=self.refresh).place(x=180, y=220)
+        Button(self.window, text="Refresh", width=7, command=self.refresh).place(x=20, y=220)
         Button(self.window, text="Save", width=7, command=self.save_click).place(x=20, y=260)
         Button(self.window, text="Edit", width=7, command=self.edit_click).place(x=100, y=260)
         Button(self.window, text="Delete", width=7, command=self.delete_click).place(x=180, y=260)
@@ -74,7 +73,7 @@ class Visit:
         self.date_time.clear()
         self.description.clear()
         status, visit_list = VisitController.find_all()
-        self.tabel.refresh_table(visit_list)
+        self.table.refresh_table(visit_list)
 
     def select_from_table(self, selected_visit):
         if selected_visit:
